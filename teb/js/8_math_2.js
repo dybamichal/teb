d4d668928ea8ac6df247f14d3df9fad3bdbc5d97
dybamichal <michaldyba25@gmail.com>
2018-02-06 15:31:14 +0100

    321

---
+++ teb/js/8_math_2.js
@@ -0,0 +1,23 @@
+var pi = Math.PI;
+var x = prompt("Podaj wartość: ");
+console.log(Math.sqrt(x));
+var a = 13.2;
+var b = 28.5;
+var c = 1;
+var d = 3;
+var e = 0.5;
+var f = 1.1;
+
+//minimum
+
+var min = Math.min(a,b,c,d,e,f);
+console.log("Minimum: "+ min);
+
+//maximum
+var max = Math.max(a,b,c,d,e,f);
+console.log("Maximum: "+ max);
+
+//zaokrąglenie
+var g = 12.45;
+var zaokroglenie = Math.round(g);
+console.log("Zaokroglenie: "+ zaokroglenie);

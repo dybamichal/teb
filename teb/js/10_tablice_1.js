8d95dbeccbded179ccbd99734a66cec32423acff
dybamichal <michaldyba25@gmail.com>
2018-02-27 16:39:27 +0100

    dg

--- teb/js/10_tablice_1.js
+++ teb/js/10_tablice_1.js
@@ -14,3 +14,25 @@ function losowanie(){
 }
 
 losowanie();
+
+var t = ['Janusz', 0, true, 'Anna'];
+console.log(t);
+
+for(var i = 0; i<t.length; i++){
+
+//document.write(t[i] + ' ');
+document.write(i + 1 + ' element: ' + t[i] + '<br>');
+
+}
+
+//uzytk podaje z lapy w polu numeryczym wartosci ktore sa zapisywane do tablicy, wyswietl wszystkie wartosci z tablicy, wyswietl, ile jest ich lacznie, jaka jest ich suma oraz srednia wartosc z dwoma miejscami po przecinku
+
+
+var liczba = document.getElementById('liczba');
+var przycisk = document.getElementById('przycisk');
+var liczby [];
+
+function dodaj(){
+alert('sad');
+}
+przycisk.addEventListener('click', dodaj);

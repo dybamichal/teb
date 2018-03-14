28a6763c06cd937c79dd41ed3d15f23e956bd9c8
dybamichal <michaldyba25@gmail.com>
2018-03-14 10:25:44 +0100

    sdff

--- teb/js/13_zwiekszanieDiva.js
+++ teb/js/13_zwiekszanieDiva.js
@@ -1,7 +1,28 @@
 var elSzer = document.getElementById('szerokosc');
 var elWys = document.getElementById('wysokosc');
 var elBlok = document.getElementById('blok');
+var elKolor = document.getElementById('color');
+/*var elLewo = document.getElementById('lewo');*/
 
-elBlok.style.backgroundColor = 'red';
-elBlok.style.height = '100px';
-elBlok.style.width = '100px';
+elBlok.style.backgroundColor = elKolor.value;
+elBlok.style.height = '0px';
+elBlok.style.width = '0px';
+
+function rozmiar() {
+	let sz = elSzer.value;
+	let w = elWys.value;
+	elBlok.style.width = sz + 'px';
+	elBlok.style.height = w + 'px';
+}
+
+function color() {
+	elBlok.style.backgroundColor = elKolor.value;
+}
+
+/*
+elLewo.value = elBlok.offsetLeft
+*/
+
+elSzer.addEventListener('change', rozmiar);
+elWys.addEventListener('change', rozmiar);
+elKolor.addEventListener('change', color);
